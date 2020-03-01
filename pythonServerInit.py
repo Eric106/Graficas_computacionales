608df31e52c1c7ed217d167d9a49bd4d914f4d0a
Eric106 <ercv106@hotmail.com>
2020-03-01 16:18:00 -0600

    01/03/2020 16:18:00

---
+++ pythonServerInit.py
@@ -0,0 +1,11 @@
+from os import system, sys
+from time import sleep
+
+def main():
+    try:
+        system("python -m http.server 80 --bind 127.0.0.1")
+    except KeyboardInterrupt:
+        print("Cerrando server")
+        sleep(2)
+        sys.exit()
+main()
