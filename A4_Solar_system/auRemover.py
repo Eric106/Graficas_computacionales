aef784d5aa5b88f6e882364c60c71f03e0691856
Eric106 <ercv106@hotmail.com>
2020-03-29 23:17:53 -0600

    29/03/2020 23:17:49

--- A4_Solar_system/auRemover.py
+++ A4_Solar_system/auRemover.py
@@ -1,9 +1,9 @@
-fileName = 'js/OrbitControls.js'
+fileName = 'main.js'
 openFile = open(fileName,'r')
 lineas = openFile.readlines()
 cleanText = ''
 for linea in lineas:
-    if not "* @author" in linea:
+    if not "//" in linea or not "/*" in linea:
         cleanText += linea
 openFile.close()
 cleanFile = open('cleanJS.txt','w')
